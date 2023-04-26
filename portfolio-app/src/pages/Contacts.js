e28a2c8fd3d3a53f6fa98210554d664e0299071c
yaroprof <crosskyiv@gmail.com>
2023-04-26 17:21:34 +0300

    add contact item

--- portfolio-app/src/pages/Contacts.js
+++ portfolio-app/src/pages/Contacts.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
 
 const Contacts = () => {
   return (
-    <div>Contacts</div>
-  )
-}
+    <main className="section">
+      <div className="container">
+        <h1 className="title-1">Contacts</h1>
+        <ul className="content-list">
+          <li className="content-list__item">
+            <h2 className="title-2">Location</h2>
+            <p>Kyiv, Ukraine</p>
+          </li>
+          <li className="content-list__item">
+            <div className="title-2"></div>
+            <p>
+              <a href="#"></a>
+            </p>
+          </li>
+          <li className="content-list__item">
+            <div className="title-2"></div>
+            <p>
+              <a href="mailto:crosskyiv@gmail.com">crosskyiv@gmail.com</a>
+            </p>
+          </li>
+        </ul>
+      </div>
+    </main>
+  );
+};
 
-export default Contacts
+export default Contacts;
