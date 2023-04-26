350f15207fd7f946ee68026064d4292fb24753e0
yaroprof <crosskyiv@gmail.com>
2023-04-26 17:11:58 +0300

    create project list

---
+++ portfolio-app/src/components/btnGitHub/BtnGitHub.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import gitHubIcon from './gitHub-black.svg'
+
+const BtnGitHub = ({link}) => {
+  return (
+    <a href={link} target='_blank' rel="noreferrer" className='btn-outline'>
+      <img src={gitHubIcon} />
+      GitHub repo
+    </a>
+  )
+}
+
+export default BtnGitHub
--- portfolio-app/src/pages/Project-page.js
+++ portfolio-app/src/pages/Project-page.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import BtnGitHub from '../components/btnGitHub/BtnGitHub';
+import { useParams } from 'react-router-dom';
+import { projects } from './../helpers/projectsList';
 
 const Project = () => {
-  return (
-    <div>Project-page</div>
-  )
-}
+  const { id } = useParams();
+  const project = projects[id];
 
-export default Project
+  return (
+    <main className="section">
+      <div className="container">
+        <div className="project-details">
+          <h1 className="title-1">{project.title}</h1>
+          <img src={project.imgBig} alt={project.title} className="project-details__cover" />
+          <div className="project-details__desc">
+            <p>Skills: {project.skills}</p>
+          </div>
+          {project.gitHubLink && <BtnGitHub link="https://github.com" />}
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Project;
