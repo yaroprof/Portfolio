05c3a0b85864e13d97358ba5333b57e3434807b1
yaroprof <crosskyiv@gmail.com>
2023-05-05 16:54:41 +0300

    final

--- md-blog/models/article.js
+++ md-blog/models/article.js
@@ -1,10 +1,9 @@
-// article.js - це модуль Node.js, який визначає схему (структуру) статей блогу і їхній метод збереження в базі даних. Модуль використовує пакет mongoose для взаємодії з MongoDB.
-
-// В модулі article.js описується схема статей за допомогою mongoose.Schema, яка включає поля, такі як заголовок, автор, дата публікації і текст статті. Потім оголошується модель Article, що відповідає колекції документів articles в MongoDB.
-
-// Модуль article.js містить функції для взаємодії з базою даних, такі як збереження статті у базу даних за допомогою методу .save(), отримання списку всіх статей з бази даних за допомогою методу .find(), отримання статті за її ID за допомогою методу .findById(), тощо.
-
 const mongoose = require('mongoose')
+// const marked = require('marked')
+// const slugify = require('slugify')
+// const createDomPurify = require('dompurify')
+// const { JSDOM } = require('jsdom')
+// const dompurify = createDomPurify(new JSDOM().window)
 
 const articleSchema = new mongoose.Schema({
   title: {
@@ -34,31 +33,5 @@ const articleSchema = new mongoose.Schema({
 })
 
 
-// const Article = mongoose.model('Article', articleSchema);
 
 module.exports = mongoose.model('Article', articleSchema)
-
-
-
-
-// IMPORT
-// const marked = require('marked')
-// const slugify = require('slugify')
-// const createDomPurify = require('dompurify')
-// const { JSDOM } = require('jsdom')
-// const dompurify = createDomPurify(new JSDOM().window)
-
-
-// CONCLUSIONS
-
-// articleSchema.pre('validate', function(next) {
-//   if (this.title) {
-//     this.slug = slugify(this.title, { lower: true, strict: true })
-//   }
-
-//   if (this.markdown) {
-//     this.sanitizedHtml = dompurify.sanitize(marked(this.markdown))
-//   }
-
-//   next()
-// })
