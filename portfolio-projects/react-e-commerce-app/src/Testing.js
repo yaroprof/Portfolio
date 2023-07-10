70f6ee42a8a3d7bb7be8751cae6ef4bbefce83b3
yaroprof <crosskyiv@gmail.com>
2023-07-10 17:03:51 +0300

    nnm

---
+++ portfolio-projects/react-e-commerce-app/src/Testing.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Testing = () => {
+  return (
+    <Wrapper>
+      <h3>Hello</h3>
+      <p>hello people</p>
+      <button>click me</button> 
+    </Wrapper>
+  )
+}  
+
+const Wrapper = styled.section`
+  h3{
+    color: red;
+    
+  }
+`
+
+export default Testing
